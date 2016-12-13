242f3e7c215929245f3437386fb59f617ae41ee6
Mark Lamson <lamson_mark@yahoo.com>
2016-12-13 18:16:37 -0500

    moving to github

--- spec/OrderedJobs.spec.ts
+++ spec/OrderedJobs.spec.ts
@@ -1,7 +1,73 @@
 import {OrderedJobs} from "../src/OrderedJobs";
 
-describe('test', () => {
-    it('true should be true', () => {
-        expect(OrderedJobs.isWorking).toBe(true);
+describe('Ordered Jobs', () => {
+    it('empty joblist should return empty string', () => {
+        expect(OrderedJobs.putInOrder("")).toEqual("");
+    });
+    it('joblist with single job should return job', () => {
+        expect(OrderedJobs.putInOrder("a =>  ")).toEqual("a");
+    });
+    it('joblist with multiple jobs should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb =>  \nc =>  ")).toEqual("abc");
+    });
+    it('joblist with multiple jobs and one dependency should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc =>  ")).toEqual("acb");
+    });
+    it('joblist with multiple jobs and multiple dependency should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => d\nd =>  ")).toEqual("adcb");
+    });
+    it('joblist dependent on self should return error', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => c\nd =>  ")).toEqual("error: job self dependent");
+    });
+    it('joblist with circular dependency should return error', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => d\nd => b")).toEqual("error: circular dependency");
+    });
+    it('Empty job list should return empty string', () => {
+        expect(OrderedJobs.putInOrder("")).toEqual("");
+    });
+    it('one job no dependency should return job', () => {
+        expect(OrderedJobs.putInOrder("a =>  ")).toEqual("a");
+    });
+    it('multiple jobs no dependency should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb =>  \nc =>  ")).toEqual("abc");
+    });
+    it('multiple jobs one dependency should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc =>  ")).toEqual("acb");
+    });
+    it('multiple jobs multiple dependencies should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => d\nd =>  ")).toEqual("adcb");
+    });
+    it('empty job list should return empty string', () => {
+        expect(OrderedJobs.putInOrder("")).toEqual("");
+    });
+    it('single job in job list should return job', () => {
+        expect(OrderedJobs.putInOrder("a =>  ")).toEqual("a");
+    });
+    it('multiple jobs in job list with no dependencies should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb =>  \nc =>  ")).toEqual("abc");
+    });
+    it('multiple jobs in job list with one dependencies should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc =>  ")).toEqual("acb");
+    });
+    it('multiple jobs in job list with multiple dependencies should return jobs in order', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => a")).toEqual("acb");
+    });
+    it('multiple jobs in job list with multiple dependencies that have circular dependency should return error message', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => d\nd => b")).toEqual("error: circular dependency");
+    });
+    it('should return error if job has circular dependency', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc =>  f\nd => a\ne => \nf => b")).toEqual("error: circular dependency");
+    });
+    it('should return error if job has circular dependency', () => {
+        expect(OrderedJobs.putInOrder("a => b\nb => c\nc => a")).toEqual("error: circular dependency");
+    });
+    it('Jobs dependent on self should throw error', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => c")).toEqual("error: job self dependent");
+    });
+    it('should return job list in order for multiple jobs multiple depencencies', () => {
+        expect(OrderedJobs.putInOrder("a =>  \nb => c\nc => f\nd => a\ne => b\nf =>  ")).toEqual("afcbed"); //"afcbde"
+    });
+    it('should return multiple jobs in order from jobs list input and multiple dependecies', () => {
+        expect(OrderedJobs.putInOrder("a => d\nb => c\nc => a\nd =>  ")).toEqual("dacb");
     });
 });
--- src/OrderedJobs.ts
+++ src/OrderedJobs.ts
@@ -1,3 +1,74 @@
 export class OrderedJobs {
-    static isWorking = true;
+    static putInOrder(jobList: string): string {
+        let splitJobs: string[] = jobList.split("\n");
+        let jobsWithDependencies: string[] = [];
+        let finalJobsList: string = "";
+        let jobAdded: boolean = true;
+
+        if (this.jobListEmpty(jobList)) {
+            return "";
+        }
+
+        for (let job of splitJobs) {
+            this.breakdownSplitJobs(job, jobsWithDependencies, finalJobsList);
+        }
+        while (jobAdded) {
+            let currentJobListLength: number = finalJobsList.length;
+            for (let i: number = 0; i < jobsWithDependencies.length; i++) {
+                if (this.jobDependencyMet(finalJobsList, jobsWithDependencies, i)) {
+                    finalJobsList += jobsWithDependencies[i][0];
+                    jobsWithDependencies.splice(i, 1);
+                }
+            }
+            if (this.jobNotAdded(currentJobListLength, finalJobsList)) {
+                jobAdded = false;
+            }
+        }
+        if (this.dependentJobsNotAdded(jobsWithDependencies)) {
+            return "error: circular dependency";
+        }
+        return finalJobsList;
+    }
+
+    private static breakdownSplitJobs(job, jobsWithDependencies: string[], finalJobsList: string) {
+        if (this.jobDependentOnSelf(job)) {
+            this.jobSelfDependentMessage();
+        }
+        if (this.jobHasDependency(job)) {
+            jobsWithDependencies.push(job);
+        }
+        else {
+            finalJobsList += job[0];
+        }
+        return finalJobsList;
+    }
+
+    private static jobSelfDependentMessage() {
+        return "error: job self dependent";
+    }
+
+    private static dependentJobsNotAdded(jobsWithDependencies: string[]) {
+        return jobsWithDependencies.length > 0;
+    }
+
+    private static jobNotAdded(currentJobListLength: number, finalJobsList: string) {
+        return currentJobListLength === finalJobsList.length;
+    }
+
+    private static jobDependencyMet(finalJobsList: string, jobsWithDependencies: string[], i: number) {
+        return finalJobsList.indexOf(jobsWithDependencies[i][5]) !== -1;
+    }
+
+    private static jobHasDependency(job) {
+        return job[5] !== " ";
+    }
+
+    private static jobDependentOnSelf(job) {
+        return job[0] === job[5];
+    }
+
+    private static jobListEmpty(jobList: string) {
+        return jobList === "";
+    }
+
 }
